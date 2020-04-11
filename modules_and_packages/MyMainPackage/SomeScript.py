f1a04ac93ee779ba927bf684780c9cd21471004f
luukdej <l.dejonge@live.nl>
2020-04-11 21:09:11 +0200

    package and modules

---
+++ modules_and_packages/MyMainPackage/SomeScript.py
@@ -0,0 +1,2 @@
+def report_main():
+    print("Im a function in a SomeScript (Main) in main package")
---
+++ modules_and_packages/myprogram.py
@@ -0,0 +1,9 @@
+from mymodule import my_func
+from MyMainPackage import SomeScript
+from MyMainPackage.SubPackage import mysubscript
+
+my_func()
+
+SomeScript.report_main()
+
+mysubscript.sub_report()
