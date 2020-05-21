52c1afaf2be0d395f84b696e7c6bf5290fa97f59
Luuk <l.dejonge@live.nl>
2020-05-21 20:48:45 +0200

    simple bank manager

--- final_capstone_projects/bank_manager.py
+++ final_capstone_projects/bank_manager.py
@@ -1,19 +1,84 @@
-class Account():
+class Account:
 
-    def __init__(self, customer_id, balance=0):
+    def __init__(self, id, balance):
 
-        self.customer_id = customer_id
+        self.id = id
         self.balance = balance
 
-class CheckingAccount(Account):
+    def get_id(self):
+        return self.id
 
-    def __init__(self, customer_id, deposit_amount, withdrawal_amount):
+    def get_balance(self):
+        return self.balance
 
-        self.deposit_amount = deposit_amount
-        self.withdrawal_amoutn = withdrawal_amount
-        
-    def deposit(self, balance, deposit_amount):
+    def desposit(self, amount):
+        self.balance = self.balance + amount
 
-        self.balance = self.balance + deposit_amount
-        print("You deposited ${} to your account".format(deposit_amount))
-        print("Your total balance is now ${}".format(balance))
+    def withdrawal(self, amount):
+        self.balance = self.balance - amount
+            
+
+def main():
+
+    accounts = []
+    for i in range(1000, 9999):
+        account = Account(i, 0)
+        accounts.append(account)
+
+    while True:
+
+        print("\nHello, welcome to ABC Bank")
+
+        id = int(input("\nEnter your PIN please: "))
+
+        while id <1000 or id >9999:
+            id = int(input("\n Incorrect PIN, try again: "))
+    
+        while True:
+
+            print("\n1 - View Balance \t 2 - Withdraw \t 3 - Deposit \t 4 - Exit")
+
+            selection = int(input("\nPlease make your choice: "))
+
+            for acc in accounts:
+                if acc.get_id() == id:
+                    account_object = acc
+                    break
+
+            if selection == 1:
+                print("\nYour balance is: $" + str(account_object.get_balance()) + " ")
+
+            elif selection == 2:
+                amt = float(input("\nEnter amount to withdraw: "))
+                verify_withdraw = input("Please verify this is the correct amount. Yes/No: $" +str(amt) + "  ")
+
+                if verify_withdraw == "Yes":
+                    print("Withdrawing...")
+                else:
+                    break
+
+                if amt < account_object.get_balance():
+                    account_object.withdrawal(amt)
+                    print("\nNew Balance: $" + str(account_object.get_balance()) + "")
+                else:
+                    print("You have exceeded your balance. Retry with lower withdrawl amount")
+
+            elif selection == 3:
+                amt = float(input("\n Enter amount to deposit: "))
+
+                verify_deposit = input("Please verify this is the correct amount. Yes/No: $" + str(amt) + "  ")
+
+                if verify_deposit == "Yes":
+                    account_object.desposit(amt)
+                    print(type(account_object))
+                    print("Deposit successful.")
+                else:
+                    break
+
+            elif selection == 4: 
+                print("\nThank you for choosing ABC")
+                exit()
+
+            else:
+                print("Invalid choice")
+main()
